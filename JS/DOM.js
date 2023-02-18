71c4b8f1031ca0db734704d92fbddc6f01dc91e2
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-18 18:24:34 +0600

    explore getElementsByClassName and getElementById

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,2 +1,28 @@
 // console.log('explore DOM')
 // console.log(document)
+
+//* getElementsByTagName()
+// const li = document.getElementsByTagName('li');
+        // let i = 0;
+        // while (i < li.length) {
+        //     li[i].style.color = 'red';
+        //     console.log(li[i].innerText)
+        //     i++;
+        // }
+
+
+//* getElementById()
+document.getElementById('german-title').style.color = 'coral';
+// console.log(document.getElementById('japanese-title').innerText)
+document.getElementById('american-title').style.backgroundColor = 'lightblue';
+document.getElementById('japanese-title').innerText = 'JDM cars';
+
+//* getElementsByClassName()
+const topBrands = document.getElementsByClassName('top-brand');
+// for(let i = 0; i < topBrands.length; i++){
+//     topBrands[i].style.backgroundColor = 'green'
+//     topBrands[i].style.color = 'white'
+//     topBrands[i].style.padding = '10px 10px'
+// }
+
+)
