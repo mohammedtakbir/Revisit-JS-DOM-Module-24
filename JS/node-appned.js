3e733a32b76a9f8e5cf67305aeea650646dd2b9b
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-18 23:45:15 +0600

    explore createElement(), appendChild(), parentNode()

---
+++ JS/node-appned.js
@@ -0,0 +1,26 @@
+//* childNodes() 
+// const usa = document.querySelector('.usa').childNodes[3];
+// const usa = document.querySelector('.usa').childNodes[3].childNodes[1];
+// console.log(usa)
+
+//* children() ---> returns htmlCollections
+// const usa = document.querySelector('.usa');
+// const usa = document.querySelector('.usa').children[1].childNodes;
+
+// const usa = document.querySelector('.usa').children[1].children[3].nextSibling;
+
+//! don't working
+// const usa = document.querySelector('.usa').children[1].children[3].previousSibling;
+// console.log(usa)
+
+//* createElement();
+const usa = document.querySelector('.usa').childNodes[3];
+const li = document.createElement('li');
+const li2 = document.createElement('li');
+li.innerText = 'This is new born li tag';
+li2.innerText = 'Second born li tag';
+usa.appendChild(li);
+usa.appendChild(li2)
+
+//* parentNode
+console.log(usa.parentNode.parentNode.parentNode.parentNode)
